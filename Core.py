680f36346a23a381417635108dd29b7b2ac10309
hassan <h.zaker2222@gmail.com>
2021-03-07 12:24:49 +0330

    added user interface

--- Core.py
+++ Core.py
@@ -2,7 +2,8 @@ from lark import Lark
 from Grammer import grammer
 from TreeProcessor import Calculator
 
-parser = Lark(grammer, start="exp", parser="earley", lexer="standard")
-calculator = Calculator()
-answer = calculator.transform(parser.parse('(3 * -4) + sqrt(4)'))
-print(answer)
+def process(text):
+    parser = Lark(grammer, start="exp", parser="earley", lexer="standard")
+    calculator = Calculator()
+    answer = calculator.transform(parser.parse(text))
+    return answer
